d44c72a65a0c558c7439ec7f57ce7d84200f0030
Aylamuradin <victoria.molina89@gmail.com>
2018-04-09 12:58:19 +0200

    commit
    Primera version control y categoria

--- FrikiAgenda/src/main/java/com/frikiagenda/control/Control.java
+++ FrikiAgenda/src/main/java/com/frikiagenda/control/Control.java
@@ -1,5 +1,50 @@
 package com.frikiagenda.control;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.frikiagenda.servicios.IServicios;
+import com.frikiagenda.model.Categoria;
+
+@Controller
 public class Control {
 
+	@Autowired
+	private IServicios servicios;
+	
+	@RequestMapping("/")
+	public ModelAndView cargaInicial() throws Exception{
+		List<Categoria> listaCategoria = servicios.read();
+		ModelAndView model = new ModelAndView("portada");
+		model.addObject(listaCategoria);
+		return model;
+	}
+	
+	@RequestMapping(value="/buscar_categoria", method=RequestMethod.GET)
+	public ModelAndView buscarCategoria(@ModelAttribute Categoria cat){
+		Categoria categoria = servicios.read(cat);
+		
+		ModelAndView model = new ModelAndView("redirect:/");
+		model.addObject(categoria);
+		
+		return model;
+	}
+	
+	@RequestMapping(value="/borrar_categoria", method=RequestMethod.GET)
+	public ModelAndView borrarCategoria(HttpServletRequest request){
+		int idCat = Integer.parseInt(request.getParameter("id"));
+		servicios.delete(idCat);
+		
+		return new ModelAndView("redirect:/");
+	}
+	
+	
 }
--- FrikiAgenda/src/main/java/com/frikiagenda/model/Categoria.java
+++ FrikiAgenda/src/main/java/com/frikiagenda/model/Categoria.java
@@ -1,5 +1,42 @@
 package com.frikiagenda.model;
 
-public class Categoria {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "idcategorias")
+	private int id;
+	private String nombre;
+	private String descripcion;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
 }
