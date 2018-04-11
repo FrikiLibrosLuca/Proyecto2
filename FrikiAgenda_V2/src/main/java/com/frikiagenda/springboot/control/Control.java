55b2872e1da829d97c6c8523bf69495db5522136
HivanGarches <ivangarcesribas@gmail.com>
2018-04-11 12:31:28 +0200

    @hivangarches @harkle7
    Logger debug y trace en control

--- FrikiAgenda_V2/src/main/java/com/frikiagenda/springboot/control/Control.java
+++ FrikiAgenda_V2/src/main/java/com/frikiagenda/springboot/control/Control.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.frikiagenda.springboot.dao.DaoCatImp;
 import com.frikiagenda.springboot.model.Categoria;
 import com.frikiagenda.springboot.servicios.IServicios;
 
@@ -21,12 +24,25 @@ import com.frikiagenda.springboot.servicios.IServicios;
 @Controller
 public class Control {
 
+	private static Logger logger;
+
+    static {
+        try {
+            logger = LogManager.getLogger(Control.class);
+        } catch (Throwable e) {
+            System.out.println("Don't work");
+        }
+    }
+    
+    
 	@Autowired
 	private IServicios servicios;
 	
 	@RequestMapping("/")
 	public ModelAndView cargaInicial() throws Exception{
+		logger.trace("Pantalla inicial");
 		List<Categoria> listaCategoria = servicios.read();
+		logger.debug("Lista recuperacion categoria" + listaCategoria.toString());
 		ModelAndView model = new ModelAndView("portada");
 		model.addObject(listaCategoria);
 		return model;
@@ -36,6 +52,8 @@ public class Control {
 	
 	@RequestMapping(value="/buscar_categoria/{id}", method=RequestMethod.GET)
 	public ModelAndView buscarCategoria(@ModelAttribute Categoria cat,@PathVariable int id){
+		logger.trace("Buscador de categorias");
+		
 		cat.setId(id);
 		Categoria categoria = servicios.read(cat);
 		
@@ -47,6 +65,8 @@ public class Control {
 	
 	@RequestMapping(value="/borrar_categoria/{id}", method=RequestMethod.GET)
 	public ModelAndView borrarCategoria(HttpServletRequest request,@PathVariable int id){
+		logger.trace("Delete categorias");
+		
 		servicios.delete(id);
 		
 		return new ModelAndView("redirect:/");
@@ -54,6 +74,8 @@ public class Control {
 	
 	@RequestMapping(value="/insertar_categoria", method=RequestMethod.GET)
 	public ModelAndView insertarCategoria(){
+		logger.trace("Acceso formulario de categorias");
+		
 		ModelAndView model = new ModelAndView("formularioCategorias");
 		model.addObject("categoria", new Categoria());
 		
@@ -62,6 +84,8 @@ public class Control {
 	
 	@RequestMapping(value="/guardar_categoria", method=RequestMethod.POST)
 	public ModelAndView guardarCategoria(@ModelAttribute Categoria cat) {
+		logger.trace("POST insert de categoria");
+		
 		servicios.insert(cat);
 		
 		ModelAndView model = new ModelAndView("redirect:/");
@@ -71,6 +95,8 @@ public class Control {
 	
 	@RequestMapping(value="/modificar_categoria/{id}", method=RequestMethod.GET)
 	public ModelAndView modificarCategoria(@ModelAttribute Categoria cat,@PathVariable int id){
+		logger.trace("Update categoria");
+		
 		cat.setId(id);
 		cat = servicios.read(cat);
 		
