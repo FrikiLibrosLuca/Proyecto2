629cb7175a443ed154068ee7f614206ee1bf7f21
Harkle7 <harkle7@gmail.com>
2018-04-12 15:19:46 +0200

    modelos mapeados

---
+++ FrikiAgenda_V4/src/main/java/com/frikiagenda/springboot/model/Departamento.java
@@ -0,0 +1,72 @@
+package com.frikiagenda.springboot.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departamentos")
+public class Departamento implements Serializable {
+	
+	
+		private static final long serialVersionUID = 1L;
+
+		@Id
+		@GeneratedValue
+		@Column(name = "iddepartamento")
+		private int id;
+		
+		
+		private String nombre;
+
+
+		public Departamento() {
+			super();
+		}
+
+
+		public Departamento(int id, String nombre) {
+			super();
+			this.id = id;
+			this.nombre = nombre;
+		}
+
+
+		public int getId() {
+			return id;
+		}
+
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+
+		public String getNombre() {
+			return nombre;
+		}
+
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+
+		public static long getSerialversionuid() {
+			return serialVersionUID;
+		}
+
+
+		@Override
+		public String toString() {
+			return "Departamento [id=" + id + ", nombre=" + nombre + "]";
+		}
+		
+		
+		
+	
+}
--- FrikiAgenda_V4/src/main/java/com/frikiagenda/springboot/model/Empleado.java
+++ FrikiAgenda_V4/src/main/java/com/frikiagenda/springboot/model/Empleado.java
@@ -3,112 +3,136 @@ package com.frikiagenda.springboot.model;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "empleados")
-public class Empleado implements Serializable{
-	
+public class Empleado implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
-	//puse todos los nombres como la bbdd para no tener que poner @columna
+
+	// puse todos los nombres como la bbdd para no tener que poner @columna
 	@Id
 	@GeneratedValue
-	int idempleados;
-	String codempleado;
-	String salario;
-	LocalDateTime fechaAlta;
-	 @JoinColumn (name="idcategorias")
-	int idCategoria;
-	int idDepartamento;
+	@Column(name = "idempleados")
+	private int idempleados;
 	
-	public Empleado(int idempleados, String codempleado, String salario, LocalDateTime fechaAlta, int idCategoria,
-			int idDepartamento) {
-		super();
-		this.idempleados = idempleados;
-		this.codempleado = codempleado;
-		this.salario = salario;
-		this.fechaAlta = fechaAlta;
-		this.idCategoria = idCategoria;
-		this.idDepartamento = idDepartamento;
-	}
+	@Column(name = "codEmpleado")
+	private String codempleado;
+	
+	@Column(name = "salario")
+	private String salario;
+	
+	@Column(name = "fechaAlta")
+	private LocalDateTime fechaAltahola;
+
+	@OneToOne(fetch=FetchType.EAGER, cascade = {CascadeType.ALL})
+	@JoinColumn(name = "idCategoria")
+	private Categoria categoria;
+	
+	
+	@OneToOne(fetch=FetchType.EAGER, cascade = {CascadeType.ALL})
+	@JoinColumn(name = "idDepartamento")
+	private Departamento departamento;
+
 
 	public Empleado() {
 		super();
 	}
 
+
+	public Empleado(int idempleados, String codempleado, String salario, LocalDateTime fechaAlta, Categoria categoria,
+			Departamento departamento) {
+		super();
+		this.idempleados = idempleados;
+		this.codempleado = codempleado;
+		this.salario = salario;
+		this.fechaAltahola = fechaAlta;
+		this.categoria = categoria;
+		this.departamento = departamento;
+	}
+
+
 	public int getIdempleados() {
 		return idempleados;
 	}
+
 
 	public void setIdempleados(int idempleados) {
 		this.idempleados = idempleados;
 	}
 
+
 	public String getCodempleado() {
 		return codempleado;
 	}
+
 
 	public void setCodempleado(String codempleado) {
 		this.codempleado = codempleado;
 	}
 
+
 	public String getSalario() {
 		return salario;
 	}
+
 
 	public void setSalario(String salario) {
 		this.salario = salario;
 	}
 
-	public LocalDateTime getFechaAlta() {
-		return fechaAlta;
+
+	public LocalDateTime getFechaAltahola() {
+		return fechaAltahola;
 	}
 
-	public void setFechaAlta(LocalDateTime fechaAlta) {
-		this.fechaAlta = fechaAlta;
+
+	public void setFechaAltahola(LocalDateTime fechaAlta) {
+		this.fechaAltahola = fechaAlta;
 	}
 
-	public int getIdCategoria() {
-		return idCategoria;
+
+	public Categoria getCategoria() {
+		return categoria;
 	}
 
-	public void setIdCategoria(int idCategoria) {
-		this.idCategoria = idCategoria;
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 
-	public int getIdDepartamento() {
-		return idDepartamento;
+
+	public Departamento getDepartamento() {
+		return departamento;
 	}
 
-	public void setIdDepartamento(int idDepartamento) {
-		this.idDepartamento = idDepartamento;
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
 	}
+
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
+
 	@Override
 	public String toString() {
 		return "Empleado [idempleados=" + idempleados + ", codempleado=" + codempleado + ", salario=" + salario
-				+ ", fechaAlta=" + fechaAlta + ", idCategoria=" + idCategoria + ", idDepartamento=" + idDepartamento
-				+ "]";
+				+ ", fechaAlta=" + fechaAltahola + ", categoria=" + categoria + ", departamento=" + departamento + "]";
 	}
 	
-	
-	
-	
 
-	
-	
-	
-	
 	
 
 }
